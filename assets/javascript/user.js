ce2b594a18e9b2e2c5be4e29782c96d402cf7ecd
Caitlin Schrader <c.n.schrader22@gmail.com>
2019-04-11 20:52:25 -0500

    working validator

--- assets/javascript/user.js
+++ assets/javascript/user.js
@@ -6,43 +6,43 @@ var config = {
     projectId: "fuel-me-now",
     storageBucket: "fuel-me-now.appspot.com",
     messagingSenderId: "523223148867"
-};
-
-firebase.initializeApp(config);
-
-var database = firebase.database();
-
-
-
-
-
-$("#submitButton").on("click", function(event){
-    event.preventDefault();
-
-    console.log('we prevented the default');
-
-   
-    
-    var name = $("#name").val().trim();
-    var email = $("#email").val().trim();
-    var phone = $("#phoneNumber").val().trim();
-    var address = $("#address").val().trim();
-    var addressTwo = $("#address2").val().trim();
-    var city = $("#city").val().trim();
-    var state = $("#state").val().trim();
-    var zip = $("#zip").val().trim();
-    var make = $("#make").val().trim();
-    var model = $("#model").val().trim();
-    var year = $("#year").val().trim();
-    var comments = $("#comments").val().trim();
-    
-    if(name.length < 5){
-        $(".error").text("have at least five characters");
-        return; 
-    };
-
-    var newUser = {
-        
+  };
+  
+  firebase.initializeApp(config);
+  
+  var database = firebase.database();
+  
+  $('#myForm').validate({
+    rules: {
+      name: "required",
+      email: {
+        email: true,
+        required: true
+      }
+    },
+    messages: {
+      name: 'Please enter a name',
+      email: {
+        required: 'Please enter an email',
+        email: 'Please enter a valild email'
+      }
+    },
+    submitHandler: function () {
+      var name = $("#name").val().trim();
+      var email = $("#email").val().trim();
+      var phone = $("#phoneNumber").val().trim();
+      var address = $("#address").val().trim();
+      var addressTwo = $("#address2").val().trim();
+      var city = $("#city").val().trim();
+      var state = $("#state").val().trim();
+      var zip = $("#zip").val().trim();
+      var make = $("#make").val().trim();
+      var model = $("#model").val().trim();
+      var year = $("#year").val().trim();
+      var comments = $("#comments").val().trim();
+  
+      var newUser = {
+  
         name: name,
         email: email,
         phone: phone,
@@ -55,71 +55,54 @@ $("#submitButton").on("click", function(event){
         model: model,
         year: year,
         comments: comments
-        
-        
-    };
-    
-    // database.ref().push(newUser);
-    
-    // $("#name").val("");
-    // $("#email").val("");
-    // $("#phoneNumber").val("");
-    // $("#address").val("");
-    // $("#address2").val("");
-    // $("#city").val("");
-    // $("#state").val("");
-    // $("#zip").val("");
-    // $("#make").val("");
-    // $("#model").val("");
-    // $("#year").val("");
-    // $("#comments").val("");
-    
-    
- 
-
+  
+  
+      };
+  
+      // database.ref().push(newUser);
+  
+      // $("#name").val("");
+      // $("#email").val("");
+      // $("#phoneNumber").val("");
+      // $("#address").val("");
+      // $("#address2").val("");
+      // $("#city").val("");
+      // $("#state").val("");
+      // $("#zip").val("");
+      // $("#make").val("");
+      // $("#model").val("");
+      // $("#year").val("");
+      // $("#comments").val("");
+  
+    }
   });
-
-  database.ref().on("child_added", function(childSnapshot) {
+  
+  database.ref().on("child_added", function (childSnapshot) {
     console.log(childSnapshot.val());
-
+  
     var name = childSnapshot.val().name;
-  var email = childSnapshot.val().email;
-  var phone = childSnapshot.val().phone;
-  var address = childSnapshot.val().address;
-  var addressTwo = childSnapshot.val().addressTwo;
-  var city = childSnapshot.val().city;
-  var state = childSnapshot.val().state;
-  var zip = childSnapshot.val().zip;
-  var make = childSnapshot.val().make;
-  var model = childSnapshot.val().model;
-  var year = childSnapshot.val().year;
-  var comments = childSnapshot.val().comments;
-
-  console.log(name);
-  console.log(email);
-  console.log(phone);
-  console.log(address);
-  console.log(addressTwo);
-  console.log(city);
-  console.log(state);
-  console.log(zip);
-  console.log(make);
-  console.log(model);
-  console.log(year);
-  console.log(comments);
-
-
-
+    var email = childSnapshot.val().email;
+    var phone = childSnapshot.val().phone;
+    var address = childSnapshot.val().address;
+    var addressTwo = childSnapshot.val().addressTwo;
+    var city = childSnapshot.val().city;
+    var state = childSnapshot.val().state;
+    var zip = childSnapshot.val().zip;
+    var make = childSnapshot.val().make;
+    var model = childSnapshot.val().model;
+    var year = childSnapshot.val().year;
+    var comments = childSnapshot.val().comments;
+  
+    console.log(name);
+    console.log(email);
+    console.log(phone);
+    console.log(address);
+    console.log(addressTwo);
+    console.log(city);
+    console.log(state);
+    console.log(zip);
+    console.log(make);
+    console.log(model);
+    console.log(year);
+    console.log(comments);
   });
-
-  $(document).ready(function(){
-      $("#myForm").validate({
-
-        rules: {
-            name: "required",
-            email: "required",
-        }
-      })
-  });
-    
-    
